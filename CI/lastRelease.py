9b87ada02c245ced4f458a05378e317a4c62a6e2
nikvin15 <110590898+nikvin15@users.noreply.github.com>
2024-06-19 13:19:46 +0530

    Update lastRelease.py

--- CI/lastRelease.py
+++ CI/lastRelease.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
 
 import ghApiClient
+from django.conf import settings
+
+log = logging.getLogger(__name__)
+
+access_key = settings.access_key
+secret_key = settings.secret_key
+
+
+s3Client = boto3.client('s3', aws_access_key_id=access_key,
+                        aws_secret_access_key=secret_key,
+                        region_name="ap-south-1")
 
 def getLastReleaseTag():
     content = ghApiClient.readUrl('repos/swagger-api/swagger-core/releases')
+    bankAccountId = "123213123213"
+    print(bankAccountId)
     for l in content:
         draft = l["draft"]
         tag = l["tag_name"]
